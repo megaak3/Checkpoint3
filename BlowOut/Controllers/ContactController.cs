68dec39057875eeb79fcdb17ec939472f689b7f3
Andrew Keeley <megaak3@gmail.com>
2018-11-08 09:11:50 -0700

    Add project files.

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Contact
+        public ActionResult Email(string name, string email)
+        {
+            ViewBag.content = "Thank you " + name + ". We will send an email to " + email;
+            ViewBag.email = email;
+            ViewBag.name = name;
+
+            return View();
+        }
+    }
+}
