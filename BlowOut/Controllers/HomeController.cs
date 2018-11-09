1f7c112b31f0854ce69317011b83a9206d43ce5a
Andrew Keeley <megaak3@gmail.com>
2018-11-08 19:51:32 -0700

    Where I'm at

--- BlowOut/Controllers/HomeController.cs
+++ BlowOut/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BlowOut.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "What is Blow Out all about.";
 
             return View();
         }
