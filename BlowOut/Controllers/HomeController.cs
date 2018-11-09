896ff776813bd7183c03c18736edb3b3bdb0c03a
Eric Beecher <beechereric@gmail.com>
2018-11-08 20:05:18 -0800

    Instrument View pt 2

--- BlowOut/Controllers/HomeController.cs
+++ BlowOut/Controllers/HomeController.cs
@@ -27,5 +27,43 @@ namespace BlowOut.Controllers
 
             return View();
         }
+
+        // GET: Insturment
+        public ActionResult Instrument(string id)
+        {
+            switch (id)
+            {
+                case "Trumpet":
+                    ViewBag.New = "55";
+                    ViewBag.Used = "25";
+                    break;
+                case "Trombone":
+                    ViewBag.New = "60";
+                    ViewBag.Used = "35";
+                    break;
+                case "Tuba":
+                    ViewBag.New = "70";
+                    ViewBag.Used = "50";
+                    break;
+                case "Flute":
+                    ViewBag.New = "40";
+                    ViewBag.Used = "25";
+                    break;
+                case "Clarinet":
+                    ViewBag.New = "35";
+                    ViewBag.Used = "27";
+                    break;
+                case "Saxaphone":
+                    ViewBag.New = "42";
+                    ViewBag.Used = "30";
+                    break;
+
+            }
+
+
+            ViewBag.Insturment = id;
+            ViewBag.Image = id + ".jpg";
+            return View();
+        }
     }
 }
